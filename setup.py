f37d20c1a61ac57ff71ae3f46a4ca2217b7cf64d
ipicspro <36360306+ipicspro@users.noreply.github.com>
2020-05-19 15:45:53 +0300

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(name='request-headers',
+      version='0.1',
+      url='https://github.com/ipicspro/request-headers',
+      license='MIT',
+      author='IS',
+      author_email='info@ecommaker.com',
+      description='Request header generator',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      zip_safe=False,
+      setup_requires=['nose>=1.0'],
+      test_suite='nose.collector')
